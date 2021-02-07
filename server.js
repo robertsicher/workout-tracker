f26c46a2e28e013e986bab152facf6410a7a7797
robertsicher <62100753+robertsicher@users.noreply.github.com>
2021-02-07 22:04:16 +0000

    models tweaks, server express app additions

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
-//NPM
+//NPM modules
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 //Port
 const PORT = process.env.PORT || 3000;
 
+dotenv.config();
 
 //Express App
-const app = express();
+const app = express()
+.use(logger('dev'))
+.use(express.urlencoded({ extended: true}))
+.use(express.json)
+.use(express.static('public')); 
 
-//require('./routes/html-routes.js')(app);
-//require('./routes/api-routes.js')(app);
+require('./routes/html-routes.js')(app);
+require('./routes/api-routes.js')(app);
  
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
@@ -20,5 +26,5 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 
 //App listening feedback
 app.listen(PORT, () => {
-    console.log(`App is running on port ${PORT}!`);
+    console.log(`App is running on port ${PORT}`);
   });

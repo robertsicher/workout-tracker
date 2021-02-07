c05f6b976e3801e83b29ca62a91457c534f14657
robertsicher <62100753+robertsicher@users.noreply.github.com>
2021-02-07 20:11:09 +0000

    Added the workout schema

---
+++ models/workout-model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema ({
+    day: {
+        type: Date,
+        require: true,
+        default: Date.now(),
+    },
+    exercises: {
+        type: String,
+        trim: true,
+        required: true,
+    }
+}); 
+
+const Workout = mongoose.model('Workout', workoutSchema);
+
+module.exports = Workout;
+
--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 //NPM
 const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
 //Port
 const PORT = process.env.PORT || 3000;
 
+
 //Express App
-const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+const app = express();
+
 //require('./routes/html-routes.js')(app);
 //require('./routes/api-routes.js')(app);
  
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
 //App listening feedback
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}!`);
